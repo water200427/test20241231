0b13635a64ae230a845b971df67cb5238544b9f5
Water shen <water200427@gmail.com>
2024-12-31 12:00:01 +0800

    fix some answer

--- q2.py
+++ q2.py
@@ -2,5 +2,7 @@ def string_reverse(s):
     print(s[::-1])
 
 
+if __name__ == "__main__":
+    string_reverse(s)
 # string_reverse("I am a student")
 # tneduts a ma I
--- q4.py
+++ q4.py
@@ -1,7 +1,7 @@
 def criteria_loop(n, k):
     loop = [n % (10**k)]
     for i in range(2, 10000):
-        print(f"{loop=}")
+        # print(f"{loop=}")
         r = pow(n, i, 10**k)
         if r in loop:
             print(i - 1)
@@ -13,4 +13,7 @@ def criteria_loop(n, k):
     return
 
 
-criteria_loop(123, 3)
+if __name__ == "__main__":
+    criteria_loop(32, 2)
+
+# criteria_loop(123, 3)
